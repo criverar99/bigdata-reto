270786012854458c45037177e8eb044e330c9b37
criverar99 <c.riverar99@gmail.com>
2025-03-21 19:48:48 -0600

    Updated dashboard to be seen in tabs

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-
 csv_routes = {
     "sales": "https://raw.githubusercontent.com/criverar99/bigdata-reto/refs/heads/main/Datasets/sales.csv",
     "products": "https://raw.githubusercontent.com/criverar99/bigdata-reto/refs/heads/main/Datasets/products.csv",
@@ -26,41 +25,48 @@ def cargar_datos():
 
 df = cargar_datos()
 
-# Título del Dashboard
-st.title("Mexican Toys Sales Dashboard")
+# Create tabs
+tab1, tab2 = st.tabs(["Dashboard", "Other Tab"])
 
-# Sidebar para los filtros
-st.sidebar.header("Filtros")
-mostrar_todos = st.sidebar.checkbox("Mostrar todas las ventas")
-# Filtro por producto
-productos = ["Todos los productos"] + list(df['Product_Name'].unique())
-producto_seleccionado = st.sidebar.selectbox("Seleccionar Producto", options=productos)
+with tab1:
+    # Título del Dashboard
+    st.title("Mexican Toys Sales Dashboard")
 
-# Filtro por tipo de producto
-product_type = ["Todos los tipos de productos"] + list(df['Product_Category'].unique())
-product_type_seleccionado = st.sidebar.selectbox("Seleccionar Tipo de Producto", options=product_type)
+    # Sidebar para los filtros
+    st.sidebar.header("Filtros")
+    mostrar_todos = st.sidebar.checkbox("Mostrar todas las ventas")
+    # Filtro por producto
+    productos = ["Todos los productos"] + list(df['Product_Name'].unique())
+    producto_seleccionado = st.sidebar.selectbox("Seleccionar Producto", options=productos)
 
-# Filtro por tienda
-tiendas = ["Todas las tiendas"] + list(df['Store_Name'].unique())
-tienda_seleccionada = st.sidebar.selectbox("Seleccionar Tienda", options=tiendas)
+    # Filtro por tipo de producto
+    product_type = ["Todos los tipos de productos"] + list(df['Product_Category'].unique())
+    product_type_seleccionado = st.sidebar.selectbox("Seleccionar Tipo de Producto", options=product_type)
 
-if st.sidebar.button("Filtrar"):
-    if not mostrar_todos:
-        if producto_seleccionado != "Todos los productos":
-            df = df[df['Product_Name'] == producto_seleccionado]
-        if product_type_seleccionado != "Todos los tipos de productos":
-            df = df[df['Product_Category'] == product_type_seleccionado]
-        if tienda_seleccionada != "Todas las tiendas":
-            df = df[df['Store_Name'] == tienda_seleccionada]
+    # Filtro por tienda
+    tiendas = ["Todas las tiendas"] + list(df['Store_Name'].unique())
+    tienda_seleccionada = st.sidebar.selectbox("Seleccionar Tienda", options=tiendas)
 
-# Mostrar el número total de filmes
-st.write(f"Total ventas: {len(df)}")
+    if st.sidebar.button("Filtrar"):
+        if not mostrar_todos:
+            if producto_seleccionado != "Todos los productos":
+                df = df[df['Product_Name'] == producto_seleccionado]
+            if product_type_seleccionado != "Todos los tipos de productos":
+                df = df[df['Product_Category'] == product_type_seleccionado]
+            if tienda_seleccionada != "Todas las tiendas":
+                df = df[df['Store_Name'] == tienda_seleccionada]
 
-# Mostrar la tabla filtrada
-st.dataframe(df.drop(columns=["Product_ID", "Store_ID", "Product_Price_Float", "Product_Cost_Float", "Total_Sales_Value", "Total_Cost", "Total_Profit"]))
+    # Mostrar el número total de filmes
+    st.write(f"Total ventas: {len(df)}")
 
-# Mostrar total de ventas y de ganancias
-total_ventas = df["Total_Sales_Value"].sum()
-total_ganancias = df["Total_Profit"].sum()
-st.write(f"Total de ventas: ${total_ventas:,.2f}")
-st.write(f"Total de ganancias: ${total_ganancias:,.2f}")
+    # Mostrar la tabla filtrada
+    st.dataframe(df.drop(columns=["Product_ID", "Store_ID", "Product_Price_Float", "Product_Cost_Float", "Total_Sales_Value", "Total_Cost", "Total_Profit"]))
+
+    # Mostrar total de ventas y de ganancias
+    total_ventas = df["Total_Sales_Value"].sum()
+    total_ganancias = df["Total_Profit"].sum()
+    st.write(f"Total de ventas: ${total_ventas:,.2f}")
+    st.write(f"Total de ganancias: ${total_ganancias:,.2f}")
+
+with tab2:
+    st.write("This is another tab.")
